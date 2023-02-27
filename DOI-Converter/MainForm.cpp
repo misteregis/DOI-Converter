bbe3768057c235c6b1342430dbcaa79300d3fe83
Reginaldo Izidório <misteregis@gmail.com>
2023-02-27 13:40:14 -0300

    Created form

---
+++ DOI-Converter/MainForm.cpp
@@ -0,0 +1,12 @@
+#include "MainForm.h"
+
+using namespace System;
+using namespace ::Windows::Forms;
+
+[STAThread]
+void main()
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	Application::Run(gcnew DOIConverter::MainForm());
+}
